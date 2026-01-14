0555fac83bdcc8927bea1ce1a86927f3cdd5d8b5
FaridGhazy <63331958+faridGhazy@users.noreply.github.com>
2026-01-14 14:57:19 +0700

    create build_push_image and update env

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,14 @@
+# Hentikan script jika terjadi error
+set -e
+
+# Nama image Docker
+IMAGE_NAME=goldenbird29/karsajobs-ui:latest
+
+# Build Docker image
+docker build -t $IMAGE_NAME .
+
+# Login ke Docker Hub menggunakan env variable
+echo $DOCKER_TOKEN | docker login -u goldenbird29 --password-stdin
+
+# Push image ke Docker Hub
+docker push $IMAGE_NAME
